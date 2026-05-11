f2bb991f7b03bbfcc8059f6e87e3aa226dc3a03a
Timur_Edilbaev <tedilbaev192@gmail.com>
2026-05-11 17:59:24 +0400

    Add JwtAuthenticationFilter

--- src/main/java/com/project/hotelbooking/configs/SecurityConfig.java
+++ src/main/java/com/project/hotelbooking/configs/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -12,11 +13,15 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -26,7 +31,8 @@ public class SecurityConfig {
                     session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(auth -> auth.
                     requestMatchers("/api/auth/**").permitAll()
-                    .anyRequest().authenticated());
+                    .anyRequest().authenticated())
+            .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 
--- src/main/java/com/project/hotelbooking/controllers/HotelController.java
+++ src/main/java/com/project/hotelbooking/controllers/HotelController.java
@@ -5,6 +5,7 @@ import com.project.hotelbooking.dto.response.HotelResponse;
 import com.project.hotelbooking.services.HotelService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +19,7 @@ public class HotelController {
     private final HotelService hotelService;
 
     @PostMapping("/create")
+    @PreAuthorize("hasRole('ADMIN')")
     public HotelResponse createHotel(@Valid @RequestBody HotelRequest hotelRequest) {
         return hotelService.createHotel(hotelRequest);
     }
